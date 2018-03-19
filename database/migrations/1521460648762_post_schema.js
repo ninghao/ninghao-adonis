dacbcc272f72fb489530beaf3095e28d871559f8
wanghao8080 <117663444@qq.com>
2018-03-19 20:00:02 +0800

    创建 migration 添加新的数据表

---
+++ database/migrations/1521460648762_post_schema.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class PostSchema extends Schema {
+  up () {
+    this.create('posts', (table) => {
+      table.increments()
+      table.string('title')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('posts')
+  }
+}
+
+module.exports = PostSchema
