2b47b4acbcfa9488020f780d73f5abd2c3aef666
wanghao8080 <117663444@qq.com>
2018-03-20 20:28:03 +0800

    读取数据：显示单个内容资源

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -10,13 +10,20 @@ class PostController {
     return view.render('post.create')
   }
 
-  async store ({ request }) {
+  async store ({ request, response }) {
     const newPost = request.only(['title', 'content'])
     const postID = await Database.insert(newPost).into('posts')
     console.log('postID: ', postID)
+    return response.redirect(`/posts/${ postID[0] }`)
   }
 
-  async show () {
+  async show ({ view, params }) {
+    const post = await Database
+      .from('posts')
+      .where('id', params.id)
+      .first()
+
+    return view.render('post.show', { post })
   }
 
   async edit () {
