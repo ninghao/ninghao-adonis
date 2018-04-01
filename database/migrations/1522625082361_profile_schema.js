1e54c03f401b198eb85b9ec78523800b565a1df5
wanghao8080 <117663444@qq.com>
2018-04-02 07:53:31 +0800

    一对一关系：准备模型与数据

---
+++ database/migrations/1522625082361_profile_schema.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class ProfileSchema extends Schema {
+  up () {
+    this.create('profiles', (table) => {
+      table.increments()
+      table.string('github')
+      table.integer('user_id').unsigned()
+      table.foreign('user_id').references('users.id')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('profiles')
+  }
+}
+
+module.exports = ProfileSchema
---
+++ database/seeds/ProfileSeeder.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| ProfileSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+const Factory = use('Factory')
+const Profile = use('App/Models/Profile')
+
+class ProfileSeeder {
+  async run () {
+    const profiles = [
+      { github: 'wanghao8080', user_id: 1 },
+      { github: 'xiaoxue8080', user_id: 2 }
+    ]
+
+    await Profile.createMany(profiles)
+  }
+}
+
+module.exports = ProfileSeeder
