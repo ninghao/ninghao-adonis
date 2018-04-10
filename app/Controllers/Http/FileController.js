b25fbc9ae7fcf207d2a8a518bdc980d2d00fa1c6
wanghao8080 <117663444@qq.com>
2018-04-10 16:23:49 +0800

    文件上传：路由，控制器，视图

---
+++ app/Controllers/Http/FileController.js
@@ -0,0 +1,27 @@
+'use strict'
+
+class FileController {
+  async index () {
+  }
+
+  async create ({ view }) {
+    return view.render('file.create')
+  }
+
+  async store () {
+  }
+
+  async show () {
+  }
+
+  async edit () {
+  }
+
+  async update () {
+  }
+
+  async destroy () {
+  }
+}
+
+module.exports = FileController
--- start/routes.js
+++ start/routes.js
@@ -19,6 +19,11 @@ const Profile = use('App/Models/Profile')
 Route.on('/').render('welcome')
 
 Route
+  .get('upload', 'FileController.create')
+  .as('upload')
+Route.resource('files', 'FileController')
+
+Route
   .post('logout', 'AuthController.logout')
   .as('logout')
 
