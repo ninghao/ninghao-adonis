cf7758e483d68c2516ffd80016b9602554bb88b9
wanghao8080 <117663444@qq.com>
2018-04-14 11:30:20 +0800

    分页器页码链接

--- start/hooks.js
+++ start/hooks.js
@@ -1,7 +1,13 @@
 const { hooks } = require('@adonisjs/ignitor')
+const { range } = require('lodash')
 
 hooks.after.providersBooted(() => {
   const View = use('View')
+
+  View.global('pageItems', (lastPage, page) => {
+    const allPageItems = range(1, lastPage + 1)
+    return allPageItems
+  })
 
   View.global('parseInt', (value) => {
     return parseInt(value)
