1e600ae14822273c04b86297c8a3449a4c3aba00
wanghao8080 <117663444@qq.com>
2018-04-17 15:43:25 +0800

    邮件分享文章内容（准备数据）

---
+++ app/Controllers/Http/ShareController.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const Post = use('App/Models/Post')
+
+class ShareController {
+  async email ({ auth, params, request, response, session }) {
+    const user = await auth.getUser()
+
+    switch (params.type) {
+      case 'post':
+        const post = await Post.find(params.id)
+        const author = await post.user().fetch()
+
+        return {
+          post,
+          author,
+          user
+        }
+    }
+  }
+}
+
+module.exports = ShareController
