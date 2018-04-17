5625dbcb4978dd3fa5d7f68301f55b69532ecbbe
wanghao8080 <117663444@qq.com>
2018-04-17 16:31:44 +0800

    邮件附件

--- app/Controllers/Http/ShareController.js
+++ app/Controllers/Http/ShareController.js
@@ -2,6 +2,9 @@
 
 const Post = use('App/Models/Post')
 const Mail = use('Mail')
+const File = use('App/Models/File')
+const Helpers = use('Helpers')
+const Env = use('Env')
 
 class ShareController {
   async email ({ auth, params, request, response, session }) {
@@ -25,7 +28,7 @@ class ShareController {
             host: request.header('host'),
             post: post.toJSON(),
             user: user.toJSON()
-          }, 
+          },
           (message) => {
             message
               .to(user.email)
@@ -33,6 +36,19 @@ class ShareController {
               .subject(`《${ post.title }》 - ${ author.username }`)
           }
         )
+      case 'file':
+        const file = await File.find(params.id)
+        const filePath = `${ Helpers.publicPath('uploads') }/${ file.file_name }`
+
+        await Mail.raw(`${ file.client_name }`, (message) => {
+          message
+            .to(user.email)
+            .from(Env.get('SITE_MAIL'))
+            .subject(file.client_name)
+            .attach(filePath, {
+              filename: file.client_name
+            })
+        })
     }
 
     session
