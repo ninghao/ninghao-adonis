f724db299359974741f0477557bbf96a75af37fc
wanghao8080 <117663444@qq.com>
2018-04-26 14:06:05 +0800

    更新用户帐户信息

--- app/Controllers/Http/ProfileController.js
+++ app/Controllers/Http/ProfileController.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const { validateAll } = use('Validator')
+
 class ProfileController {
   async index () {
   }
@@ -18,7 +20,34 @@ class ProfileController {
     return view.render('user.settings.profile.edit', { user: auth.user.toJSON() })
   }
 
-  async update () {
+  async update ({ request, response, auth, session }) {
+    const rules = {
+      username: `required|unique:users,username,id,${ auth.user.id }`,
+      email: `required|email|unique:users,email,id,${ auth.user.id }`,
+      github: `unique:profiles,github,user_id,${ auth.user.id }`
+    }
+
+    const validation = await validateAll(request.all(), rules)
+
+    if (validation.fails()) {
+      session
+        .withErrors(validation.messages())
+        .flashAll()
+      return response.redirect('back')
+    }
+
+    const { username, email, github } = request.all()
+    auth.user.merge({ username, email })
+    await auth.user.save()
+    await auth.user.profile().update({ github })
+
+    session
+      .flash({
+        type: 'success',
+        message: 'Profile successfully updated.'
+      })
+
+    return response.redirect('back')
   }
 
   async destroy () {
