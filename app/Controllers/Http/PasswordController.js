b77120dadbcf2099c5fc89ca4e4c6f1d09e42662
wanghao8080 <117663444@qq.com>
2018-04-27 09:31:29 +0800

    自定义表单验证规则（Hash.verify 验证 Hash 值）

--- app/Controllers/Http/PasswordController.js
+++ app/Controllers/Http/PasswordController.js
@@ -1,11 +1,31 @@
 'use strict'
 
+const { validateAll } = use('Validator')
+const Hash = use('Hash')
+
 class PasswordController {
   async edit ({ view }) {
     return view.render('user.settings.password.edit')
   }
 
-  async update () {
+  async update ({ request, response, session, auth }) {
+    const rules = {
+      old_password: `required|hashVerified:${ auth.user.password }`
+    }
+
+    const messages = {
+      'old_password.hashVerified': 'Password is invalid'
+    }
+
+    const validation = await validateAll(request.all(), rules, messages)
+
+    if (validation.fails()) {
+      session
+        .withErrors(validation.messages())
+        .flashAll()
+
+      return response.redirect('back')
+    }
   }
 }
 
