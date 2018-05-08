e2b09069f11a405203bb7741d6d21683a900e4b7
wanghao8080 <117663444@qq.com>
2018-05-08 16:21:05 +0800

    自动化同步浏览器变化（使用 Browser-Sync 创建 gulp 任务）

--- app/Controllers/Http/AntlDemoController.js
+++ app/Controllers/Http/AntlDemoController.js
@@ -3,8 +3,8 @@
 class AntlDemoController {
   async demo ({ view, antl, locale }) {
     return view.render('demo.antl', {
-      greeting: antl.formatMessage('demo.greeting', { name: 'wanghao' }),
-      locale
+      locale,
+      message: 'hello'
     })
   }
 }
