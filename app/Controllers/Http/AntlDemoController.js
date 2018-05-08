0d1755d23318d7484d37eeb008e91a32833d74eb
wanghao8080 <117663444@qq.com>
2018-05-08 16:42:02 +0800

    ICU 信息：货币

--- app/Controllers/Http/AntlDemoController.js
+++ app/Controllers/Http/AntlDemoController.js
@@ -4,12 +4,20 @@ class AntlDemoController {
   async demo ({ view, antl, locale }) {
     return view.render('demo.antl', {
       locale,
-      message: antl.formatNumber(0.33333, {
-        // minimumIntegerDigits: 2,
-        // minimumFractionDigits: 2,
-        // maximumFractionDigits: 2,
-        style: 'percent'
-      })
+      // message: antl.formatNumber(0.33333, {
+      //   // minimumIntegerDigits: 2,
+      //   // minimumFractionDigits: 2,
+      //   // maximumFractionDigits: 2,
+      //   // style: 'percent'
+      // }),
+      // message: antl.formatNumber(0.33333, {
+      //   style: 'currency',
+      //   // currency: 'usd',
+      //   currency: 'cny',
+      //   // currencyDisplay: 'symbol',
+      //   currencyDisplay: 'name',
+      // }),
+      message: antl.formatAmount(30, 'usd')
     })
   }
 }
