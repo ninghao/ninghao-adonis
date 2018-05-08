8dee112c815a7a543be11f558f83299170b5f2ba
wanghao8080 <117663444@qq.com>
2018-05-08 16:38:51 +0800

    ICU 信息：格式化数字（整数，小数，百分数）

--- app/Controllers/Http/AntlDemoController.js
+++ app/Controllers/Http/AntlDemoController.js
@@ -4,7 +4,12 @@ class AntlDemoController {
   async demo ({ view, antl, locale }) {
     return view.render('demo.antl', {
       locale,
-      message: 'hello'
+      message: antl.formatNumber(0.33333, {
+        // minimumIntegerDigits: 2,
+        // minimumFractionDigits: 2,
+        // maximumFractionDigits: 2,
+        style: 'percent'
+      })
     })
   }
 }
