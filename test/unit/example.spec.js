3139722fcd959d388b5686a4612fcb4172f52a26
wanghao8080 <117663444@qq.com>
2018-05-15 14:05:41 +0800

    测试生命周期方法（before, beforeEach, after, afterEach）

--- test/unit/example.spec.js
+++ test/unit/example.spec.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { test } = use('Test/Suite')('Example')
+const { test, before, beforeEach, after, afterEach } = use('Test/Suite')('Example')
 
 test('make sure 2 + 2 is 4', async ({ assert }) => {
   assert.equal(2 + 2, 4)
@@ -19,4 +19,20 @@ test('demo test: notEqual', async ({ assert }) => {
 test('demo test: isAbove', async ({ assert }) => {
   const data = 6
   assert.isAbove(data, 5, 'isAbove test')
+})
+
+before(async () => {
+  // console.log('before test')
+})
+
+beforeEach(async () => {
+  // console.log('beforeEach test')
+})
+
+after(async () => {
+  // console.log('after test')
+})
+
+afterEach(async () => {
+  // console.log('afterEach test')
 })
