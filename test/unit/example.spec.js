8d26b6f27a24b58525dc2dd7e57fc5353c5da40e
wanghao8080 <117663444@qq.com>
2018-05-15 14:01:40 +0800

    断言（Assertions）

--- test/unit/example.spec.js
+++ test/unit/example.spec.js
@@ -5,3 +5,18 @@ const { test } = use('Test/Suite')('Example')
 test('make sure 2 + 2 is 4', async ({ assert }) => {
   assert.equal(2 + 2, 4)
 })
+
+test('demo test: equal', async ({ assert }) => {
+  const data = 'hello'
+  assert.equal(data, 'hello', 'equal test')
+})
+
+test('demo test: notEqual', async ({ assert }) => {
+  const data = 'hello'
+  assert.notEqual(data, '您好', 'notEqual test')
+})
+
+test('demo test: isAbove', async ({ assert }) => {
+  const data = 6
+  assert.isAbove(data, 5, 'isAbove test')
+})
